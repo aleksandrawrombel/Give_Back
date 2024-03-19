fbc4953ac90a61173bad2da519b94e134c7d7b3a
Ola <128837261+aleksandrawrombel@users.noreply.github.com>
2024-03-19 15:32:43 +0100

    added links to facebook and instagram icons

--- src/components/HomeFooter.tsx
+++ src/components/HomeFooter.tsx
@@ -8,8 +8,8 @@ const Footer = () => {
     <footer className="footer">
       <p className="copyright">Copyright by Coders Lab</p>
       <div className="footer_icons">
-        <img src={facebook} alt="facebook icon" className="facebook_icon" />
-        <img src={instagram} alt="instagram icon" className="instagram_icon" />
+        <a href="https://www.facebook.com/" target="_blank"><img src={facebook} alt="facebook icon" className="facebook_icon" /></a>
+        <a href="https://www.instagram.com/" target="_blank"><img src={instagram} alt="instagram icon" className="instagram_icon" /></a>
       </div>
     </footer>
   );
